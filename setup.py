af69d65611b137aa395a94260139ae9a2b1116a6
lecreateurfrench <jeremy.venin@gmail.com>
2016-10-31 21:43:32 +0100

    setup slightly improved

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-
+#!/usr/bin/python3
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='OpenDS18B20',
@@ -9,6 +13,7 @@ setup(
     author_email='jeremy.venin@telecom-sudparis.eu',
     url='https://github.com/lecreateurfrench/openDS18B20.git',
     #license="",
+    long_description=read('readme.md')
     packages=['open_ds18b20'],
     entry_points={
         "console_scripts": ['open_ds18b20 = open_ds18b20.__main__:main']
