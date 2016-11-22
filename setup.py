f1a594afe81f067419045f48aee437b199de6438
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2016-11-22 20:44:32 +0100

    setup

--- setup.py
+++ setup.py
@@ -9,15 +9,14 @@ def read(fname):
 
 setup(
     name='OpenDS18B20',
-    version='0.9',
+    version='1.0',
     description='DS18B20 auto-detector,reader and mailer for RPi',
     author='jeremy venin',
     author_email='jeremy.venin@telecom-sudparis.eu',
     url='https://github.com/lecreateurfrench/openDS18B20.git',
     # license="",
-        long_description=read('readme.md'),
-        packages=['open_ds18b20'],
-        entry_points={
-                "console_scripts": ['open_ds18b20 = open_ds18b20.__main__:main']
+    long_description=read('readme.md'),
+    packages=['open_ds18b20'],
+    entry_points={"console_scripts": ['open_ds18b20 = open_ds18b20.__main__:main']
         }
 )
