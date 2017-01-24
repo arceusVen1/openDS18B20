1e9b2957deee8e0f3861b07359d3a008170d58ea
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-01-24 17:19:28 +0100

    probe configuration from interface

--- open_ds18b20/console.py
+++ open_ds18b20/console.py
@@ -6,13 +6,14 @@ TODO:
 
 import sys
 import getpass
+from open_ds18b20.probe import Materials, Probe
 
 # GLOBAL--------------------------------------------------
 
 PROMPT = '> '
 
-# ---------------------------------------------------------
 
+# ---------------------------------------------------------
 
 
 def display(string):
@@ -24,7 +25,7 @@ def write_dependencies():
     Indicates what to do if you don't have the modules installed
     """
     sys.stdout.write("before continuing you should add "
-                         "\"w1-gpio\" & \"w1-therm\" to /etc/modules files\n")
+                     "\"w1-gpio\" & \"w1-therm\" to /etc/modules files\n")
 
 
 def prompt_config():
@@ -36,7 +37,7 @@ def prompt_config():
     while not settings["number"]:
         try:
             number = int(input(PROMPT))
-            if number < 15 and number >= 0:
+            if 15 > number >= 0:
                 settings["number"] = number
         except:
             sys.stdout.write("a number please !")
@@ -49,7 +50,7 @@ def prompt_config():
         "adress where emails are going to be send and sent from ?\n")
     settings["email"] = input(PROMPT)
     sys.stdout.write("password ? "
-                         "(WARNING the password will be clear in the config file)\n")
+                     "(WARNING the password will be clear in the config file)\n")
     # to hide the password in the console
     settings["password"] = getpass.getpass()
 
@@ -63,4 +64,66 @@ def prompt_config():
 
 
 def config_probe():
-    pass
+    materials = Materials()
+    configured = []
+    unconfigured = []
+    probes = []
+    materials.detect_probes()
+    for material in materials.listprobes:
+        probe = Probe(material)
+        probes.append(probe)
+        if probe.has_config():
+            probe.get_data()
+            configured.append(probe)
+        else:
+            unconfigured.append(probe)
+    display("Already configured probes :")
+    for i in range(len(configured)):
+        display(str(i) + " - " + configured[i].get_slug())
+    display("Unconfigured probes")
+    for j in range(len(unconfigured)):
+        display(str(i + j) + " - " + unconfigured[j].get_slug())
+    display("Please type in a number for the probes to configure :")
+    number = int(input(PROMPT))
+    if number > i:
+        probe = unconfigured[number - i]
+    else:
+        probe = configured[i]
+        display(probe.get_slug())
+        if probe.has_alert():
+            display("max :" + str(probe.get_max_alert()))
+            display("min :" + str(probe.get_min_alert()))
+        if probe.is_thermostated():
+            display(probe.get_moment())
+            display(probe.link_moment_temp())
+    display("pseudo ?")
+    probe.set_slug(input(PROMPT))
+    display("Alert ? (y/n)")
+    alert = input(PROMPT)
+    if alert == "y":
+        probe.set_alert(True)
+        display("max ?")
+        probe.set_max_alert(float(input(PROMPT)))
+        display("min ?")
+        probe.set_min_alert(float(input(PROMPT)))
+    display("thermostat ?")
+    thermostat = input(PROMPT)
+    if thermostat == "y":
+        temps = []
+        esc = None
+        while esc is None:
+            display("add a temp")
+            temp = float(input(PROMPT))
+            if temp == 0 and temps != []:
+                esc = 0
+            else:
+                temps.append(temps)
+        probe.set_thermostated(True, temps)
+        display("Please give the start of each time lapse in order (HH:MM)")
+        moments = []
+        for i in temps:
+            moments.append(input(PROMPT))
+        probe.set_moment(moments)
+    probe.set_data()
+    for probe in configured:
+        probe.config.close_file()
